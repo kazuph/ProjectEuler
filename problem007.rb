6249d1e9a957b8160f6383ef10f14f25c80becc8
Kazuhiro Homma <kazu.homma@gmail.com>
2012-08-11 11:10:31 +0900

    Update problem007.rb
    ウルトラ爆速にしたった！

--- problem007.rb
+++ problem007.rb
@@ -24,3 +24,27 @@ while  prime_ary.count < 10001
   end
 end
 p prime_ary[-1]
+
+__END__
+# ネットの情報と藤堂さんのコードを元にウルトラ爆速にしたった
+n = Integer(ARGV.shift || 10001)
+max = n * Math::log(n) + n * Math::log( Math::log(n) )
+max2 = (max-3)/2
+for_end = Integer((Math.sqrt(max)-3)/2)
+sieve = Array.new(max2, true)
+# たぶん素数が存在する番号をここで計算している
+for i in 0 .. for_end
+  next unless sieve[i]
+  k = i+i+3
+  j = k*(i+1)+i
+  while j <= max2
+    sieve[j] = nil
+    j += k
+  end
+end
+for i in 0 .. max2
+  sieve[i] && sieve[i]=i+i+3
+end
+sieve.compact!
+sieve.unshift(2)
+p sieve[n - 1]
