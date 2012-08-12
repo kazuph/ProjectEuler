6b045004b1ef1da17298e43c7131ede5e2577100
kazuhiro homma <kazu.homma@gmail.com>
2012-08-12 09:27:27 -0400

    5番は激速ずる版で

--- problem005.rb
+++ problem005.rb
@@ -5,20 +5,21 @@ t = Time.now
 # p 10.step(1/0.0, 10).each{|n| break n if (1..10).all?{|m|n%m==0}}.to_i
 # p 20.step(1/0.0, 20).each{|n| break n if (1..20).all?{|m|n%m==0}}.to_i
 
-def dividable?(num, n)
-  return true if n == 2
-  return false if num % (n - 1) != 0
-  dividable?(num, n - 1)
-end
-n = 20
-while true
-  if dividable?(n, 20)
-    p n.to_i
-    break
-  end
-  n += 20
-end
-p ((Time.now - t)*1000).to_i # => 2秒程度
+# def dividable?(num, n)
+#   return true if n == 2
+#   return false if num % (n - 1) != 0
+#   dividable?(num, n - 1)
+# end
+# n = 20
+# while true
+#   if dividable?(n, 20)
+#     p n.to_i
+#     break
+#   end
+#   n += 20
+# end
+# p ((Time.now - t)*1000).to_i # => 2秒程度
 
 # ここまでやって実はRubyには最小公倍数を求めるメソッドがあってそれを使うと一瞬で求められることを発見orz
 p (1..20).inject(1) {|result, n| result.lcm n}
+p ((Time.now - t)*1000).to_i
