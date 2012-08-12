a939051cf1e15b693fa073e70c06ceda31221deb
kazuhiro homma <kazu.homma@gmail.com>
2012-08-11 23:15:02 -0400

    inject(&:+)に変更したのと改行加えて読みやすくした

--- problem002.rb
+++ problem002.rb
@@ -3,7 +3,9 @@
 #  1,  2,  3,  5,  8,  13,  21,  34,  55,  89,  ...
 #  数列の項の値が400万を超えない範囲で、偶数値の項の総和を求めよ。
 def sum_of_even_fib (max)
-  return (0..1/0.0).inject([1, 2]){|fib, i| fib[i] + fib[i + 1] < max ? fib << fib[i] + fib[i + 1] : (break fib)}.select{|n|n % 2 == 0}.inject(0){|sum, n| sum + n}
+  return (0..1/0.0).inject([1, 2]){|fib, i|
+    fib[i] + fib[i + 1] < max ? fib << fib[i] + fib[i + 1] : (break fib)
+  }.select{|n|n % 2 == 0}.inject(0){&:+}
 end
 p sum_of_even_fib(10) # => 10
 p sum_of_even_fib(4_000_000) # => 4613732
