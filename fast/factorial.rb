a8d952698615bee31158bfb54020ff0aebf3e3c9
kazuhiro homma <kazu.homma@gmail.com>
2012-09-02 14:38:07 +0900

    問題2ベンチマーク

--- fast/factorial.rb
+++ fast/factorial.rb
@@ -1,11 +1,16 @@
 #!/usr/bin/env ruby
 # encoding : utf-8
 require 'benchmark'
+
 result = {}
 Benchmark.bm do |x|
-  result[:slow] = x.report("slow ") {
+  result[:**] = x.report("** ") {
+    p 2 ** 1000
   }
-  result[:fast] = x.report("fast ") {
+  result[:times] = x.report("times ") {
+    r = 1
+    1000.times{r *= 2}
+    p r
   }
 end
 result.sort_by{|key, value| value.real}.map{|key, value| print "code : #{key} = " + "%0.4f" % (value.real * 1000) + "[msec]\n"}
