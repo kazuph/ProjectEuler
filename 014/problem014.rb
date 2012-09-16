6ac5d224c5815ae94994ab03b3c9354d9bcc4a60
kazuhiro homma <kazu.homma@gmail.com>
2012-09-16 17:18:19 +0900

    リファクタ

--- 014/problem014.rb
+++ 014/problem014.rb
@@ -9,34 +9,61 @@
 # さて、100万未満の数字の中でどの数字からはじめれば一番長い数列を生成するか。
 # 注意: 数列の途中で100万以上になってもよい
 require "pp"
-t = Time.now
-@answer = 0
-@max = 0
-@collatz = Hash.new(1)
-def collatz_seriese (n)
-  seriese = n
-  while seriese != 1
-    if seriese % 2 == 0
-      seriese /= 2
-    else
-      seriese =  3 * seriese + 1
+require 'benchmark'
+result = {}
+Benchmark.bm do |x|
+  result[:slow] = x.report("slow ") {
+    @answer = 0
+    @max = 0
+    @collatz = Hash.new(1)
+    def collatz_seriese (n)
+      seriese = n
+      while seriese != 1
+          seriese % 2 == 0 ? seriese /= 2 : seriese =  3 * seriese + 1
+        if @collatz[seriese] > 1
+          @collatz[n] += @collatz[seriese]
+          return
+        end
+        @collatz[n] += 1
+      end
+    ensure
+      if @max < @collatz[n]
+        @max = @collatz[n]
+        @answer = n
+      end
     end
-    if @collatz[seriese] > 1
-      @collatz[n] += @collatz[seriese]
-      return
+    # (2..999_999).each do |num|
+    (2..999).each do |num|
+      collatz_seriese(num)
     end
-    @collatz[n] += 1
-  end
-ensure
-  if @max < @collatz[n]
-    @max = @collatz[n]
-    @answer = n
-  end
+    # pp @collatz
+    p @answer
+  }
+  result[:fast] = x.report("fast ") {
+    @answer = 0
+    @max = 0
+    @collatz = Hash.new(1)
+    # (2..999_999).each do |num|
+    (2..999).each do |n|
+      begin
+        s = n
+        while s != 1
+          s % 2 == 0 ? s /= 2 : s =  3 * s + 1
+          if @collatz[s] > 1
+            @collatz[n] += @collatz[s]
+            break
+          else
+            @collatz[n] += 1
+          end
+        end
+      ensure
+        if @max < @collatz[n]
+          @max = @collatz[n]
+          @answer = n
+        end
+      end
+    end
+    p @answer
+  }
 end
-# (2..999_999).each do |num|
-(2..999).each do |num|
-  collatz_seriese(num)
-end
-pp @collatz
-p @answer
-p Time.now  - t
+result.sort_by{|key, value| value.real}.map{|key, value| print "code : #{key} = " + "%0.4f" % (value.real * 1000) + "[msec]\n"}
--- 017/problem017.rb
+++ 017/problem017.rb
@@ -5,34 +5,77 @@
 # では 1 から 1000 (one thousand) までの数字をすべて英単語で書けば、全部で何文字になるか。
 # 注: 空白文字やハイフンを数えないこと。例えば、342 (three hundred and forty-two) は 23 文字、115 (one hundred and fifteen) は20文字と数える。なお、"and" を使用するのは英国の慣習。
 
-# 他と組み合わせない系
-first = %w(one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
-# 他と組み合わせる系
-twent = %w(twent thirt fort fift sixt sevent eight ninet )
-twent_end = %w(y y)
-one = [""] + %w( one two three four five six seven eight nine)
-hundredth = " hundred"
-hundred = " hundred and "
-number = []
-one.each do |hund|
-  # 100, 200, 300, ...
-  if hund != ""
-    number << hund + hundredth
-    hund = hund + hundred
-  end
+require 'benchmark'
+result = {}
+Benchmark.bm do |x|
+  result[:slow] = x.report("slow ") {
+    # 他と組み合わせない系
+    first = %w(one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen).map(&:length)
+    # 他と組み合わせる系
+    twent = %w(twenty thirty forty fifty sixty seventy eighty ninety ).map(&:length)
+    one = ([""] + %w( one two three four five six seven eight nine)).map(&:length)
+    hundredth = "hundred".length
+    hundred = "hundredand".length
 
-  # 1 ~ 19
-  first.each do |fir|
-    number << hund + fir
-  end
-  # 20 ~ 99
-  twent.each do |twen|
-    number << hund + twen + twent_end[0]
-    first[0..8].each do |fir|
-      number << hund + twen + twent_end[1] + " " + fir
+    number = 0
+    one.each do |hund|
+      # 100, 200, 300, ...
+      if hund != 0
+        number += (hund + hundredth)
+        hund = hund + hundred
+      end
+
+      # 1 ~ 19
+      first.each do |fir|
+        number += (hund + fir)
+      end
+      # 20 ~ 99
+      twent.each do |twen|
+        number += (hund + twen)
+        first[0..8].each do |fir|
+          number += (hund + twen + fir)
+        end
+      end
     end
-  end
+    number += "onethousand".length
+    p number
+  }
+  result[:fast] = x.report("fast ") {
+    lens = {0 => 0}
+
+    words_19 = %w(one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
+    words_19.each_with_index do |num, index|
+      lens[index+1]=num.size
+    end
+    words_10 = %w(twenty thirty forty fifty sixty seventy eighty ninety)
+    words_10.each_with_index do |num, index|
+      base = (index + 2) * 10
+      size = num.size
+      lens[base] = size
+      1.upto(9) do |pow|
+        lens[base+pow] = size + lens[pow]
+      end
+    end
+
+    sep = 'and'.size
+    hundred = 'hundred'.size
+
+    sum = 0
+
+    1.upto(999) do |num|
+      if num < 100
+        sum+=lens[num]
+      else
+        hand = hundred + lens[num / 100]
+        sum += hand
+        remainder = num % 100
+        if remainder != 0  #100の倍数
+          sum += sep + lens[remainder]
+        end
+      end
+    end
+
+    p sum + 'one'.size + 'thousand'.size
+  }
 end
-number << "one thousand"
-# number.map{|n|p n}
-p number.map{|n|n.gsub(/ /, "")}.map(&:length).inject(&:+)
+result.sort_by{|key, value| value.real}.map{|key, value| print "code : #{key} = " + "%0.4f" % (value.real * 1000) + "[msec]\n"}
